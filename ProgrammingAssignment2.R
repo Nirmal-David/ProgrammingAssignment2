94b8031642c96ccd78625234354534360ed1cf06
Nirmal David <nirmaldavid2001@gmail.com>
2020-06-06 21:10:09 +0530

    Cachematrix

---
+++ ProgrammingAssignment2.R
@@ -0,0 +1,34 @@
+##creating a list containing a function-makecachematrix
+##for setting value of the matrix
+##for getting the value of the matrix
+##for setting/getting the inverse of matrix
+
+makecachematrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+
+##returning a matrix that is inverse of "x" using cachesolve
+
+
+cachesolve <- function(x, ...) {
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
+}
